f86cf6e1cd2ff9047e77f9ef116ca9bbfef02851
Daniel Sabino <40477679+D-Sabino@users.noreply.github.com>
2025-02-06 15:28:36 -0300

    Atualizacoes do curso

--- Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Starter/Program.cs
+++ Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Starter/Program.cs
@@ -1,23 +1,3 @@
-/* 
-This C# console application is designed to:
-- Use arrays to store student names and assignment scores.
-- Use a `foreach` statement to iterate through the student names as an outer program loop.
-- Use an `if` statement within the outer loop to identify the current student name and access that student's assignment scores.
-- Use a `foreach` statement within the outer loop to iterate though the assignment scores array and sum the values.
-- Use an algorithm within the outer loop to calculate the average exam score for each student.
-- Use an `if-elseif-else` construct within the outer loop to evaluate the average exam score and assign a letter grade automatically.
-- Integrate extra credit scores when calculating the student's final score and letter grade as follows:
-    - detects extra credit assignments based on the number of elements in the student's scores array.
-    - divides the values of extra credit assignments by 10 before adding extra credit scores to the sum of exam scores.
-- use the following report format to report student grades: 
-
-    Student         Grade
-
-    Sophia:         92.2    A-
-    Andrew:         89.6    B+
-    Emma:           85.6    B
-    Logan:          91.2    A-
-*/
 int examAssignments = 5;
 
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
@@ -31,18 +11,9 @@ int[] studentScores = new int[10];
 
 string currentStudentLetterGrade = "";
 
-// display the header row for scores/grades
 Console.Clear();
-Console.WriteLine("Student\t\tGrade\tLetter Grade\n");
+Console.WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit\n");
 
-/*
-The outer foreach loop is used to:
-- iterate through student names 
-- assign a student's grades to the studentScores array
-- sum assignment scores (inner foreach loop)
-- calculate numeric and letter grade
-- write the score report information
-*/
 foreach (string name in studentNames)
 {
     string currentStudent = name;
@@ -60,27 +31,36 @@ foreach (string name in studentNames)
         studentScores = loganScores;
 
     int sumAssignmentScores = 0;
-
     decimal currentStudentGrade = 0;
-
+    
     int gradedAssignments = 0;
+    int gradedExtraCreditAssignments = 0;
 
-    /* 
-    the inner foreach loop sums assignment scores
-    extra credit assignments are worth 10% of an exam score
-    */
+    int sumExamScores = 0;
+    int sumExtraCreditScores = 0;
+
+    decimal currentStudentExamScore = 0;
+    decimal currentStudentOverallGrade = 0;
+    decimal currentStudentExtraCredit = 0;
+
+
     foreach (int score in studentScores)
     {
         gradedAssignments += 1;
 
         if (gradedAssignments <= examAssignments)
-            sumAssignmentScores += score;
+            sumExamScores = sumExamScores + score;
 
-        else
-            sumAssignmentScores += score / 10;
+        else{
+            gradedExtraCreditAssignments += 1;
+            sumExtraCreditScores += score;
+        }
     }
 
-    currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
+    currentStudentExamScore = (decimal)(sumExamScores) / examAssignments;
+    currentStudentExtraCredit = (decimal)(sumExtraCreditScores) / gradedExtraCreditAssignments;
+
+    currentStudentGrade = (decimal)((decimal)sumExamScores + ((decimal)sumExtraCreditScores / 10)) / examAssignments;
 
     if (currentStudentGrade >= 97)
         currentStudentLetterGrade = "A+";
@@ -124,7 +104,7 @@ foreach (string name in studentNames)
     // Student         Grade
     // Sophia:         92.2    A-
     
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentExamScore}\t\t{currentStudentLetterGrade}\t\t{currentStudentExtraCredit} ({(((decimal)sumExtraCreditScores / 10) / examAssignments)} pts)");
 }
 
 // required for running in VS Code (keeps the Output windows open to view results)
---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,36 @@
+﻿//int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+
+//Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+//Exercicio cara ou coroa:
+
+        // Gera um número aleatório: 0 para Cara e 1 para Coroa
+//        Random random = new Random();
+//        int resultado = random.Next(0, 2);
+        
+//        string resultadoMoeda = (resultado == 0) ? "cara" : "coroa";
+
+//        Console.WriteLine($"\nResultado: {resultadoMoeda}");
+
+        /*
+            Solucao do curso:
+            Random coin = new Random();
+            Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
+        */
+
+string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin")) {
+    if (level > 55 )
+        Console.WriteLine("Welcome, Super Admin user.");
+    else if (level <= 55)
+        Console.WriteLine("Welcome, Admin user.");
+}
+else if (permission.Contains("Manager")){
+    if (level >= 20)
+        Console.WriteLine("Contact an Admin for access.");
+    else if (level < 20)
+        Console.WriteLine("You do not have sufficient privileges.");
+}
